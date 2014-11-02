ed24424e9bacdcfeda3752f920b128a63f9d49d7
AKFish <akfish@gmail.com>
2014-11-02 22:53:14 +0800

    Initial implementation

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-var generator = require('./libs/generator');
+var async = require('async');
+var path = require('path');
+var jumly = require('./generators/jumly');
 
-var code = [
-    '@found "You", ->',
-    '  @message "get", "JUMLY"',
-  ].join('\n');
+var r = /\n*(`{3,}|~{3,}) *(.+)? *\n([\s\S]+?)\s*\1\n*/g
 
-generator('sequence', code, 'example.png', function () {
-  console.log("Done!");
-});
+var generators = {
+  'sequence': jumly,
+  'robustness': jumly
+}
+console.log("Hexo Diagram Filter")
+var base = hexo.source.base;
+var diagramFolder = path.join(base, 'diagrams');
+console.log(diagramFolder);
+
+hexo.extend.filter.register('before_post_render', function(data, callback){
+  // console.log(data.content);
+  var matches;
+  var diagrams = {};
+  var tasks = []
+  var i = 0;
+
+  function makeTask(lang, code, output, str) {
+    return function (cb) {
+      console.log("Generate: [" + lang + "]" + output);
+      var gen = generators[lang];
+      gen(lang, code, path.join(diagramFolder, output), function (err) {
+        cb(err, {src: str, output: output});
+      })
+    }
+  }
+  // Gather all diagram blocks
+  while ((matches = r.exec(data.content)) != null) {
+    var str = matches[0];
+    var lang = matches[2];
+    var code = matches[3];
+    if (lang in generators) {
+      var diagram = diagrams[str] = {
+        lang: lang,
+        code: code,
+        index: i
+      }
+      // console.log(diagram);
+      var output = data.slug + "-diagram-" + i + ".png";
+      // console.log(path.join(diagramFolder, output));
+      tasks.push(makeTask(lang, code, output, str));
+      i++;
+    }
+  }
+  // Execute generators
+  async.parallel(tasks, function (err, results) {
+    // TODO: handle error
+    // Results
+    results.forEach(function (o) {
+      diagrams[o.src].output = o.output;
+    })
+    // Replace output
+    data.raw = data.content = data.content.replace(r, function(match, ticks, lang, code, offset, str) {
+      if (match in diagrams && diagrams[match].output)
+        return "\n![](/diagrams/" + diagrams[match].output + ")\n";
+      return match;
+    });
+    // Done
+    callback(err, data);
+  });
+
+}, 1);
